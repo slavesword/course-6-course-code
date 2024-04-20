778070ea2674ee4782b177a710b17518fb2fe754
slavesword <slaven.vulic@gmail.com>
2024-04-20 18:27:21 +0100

    add file and delete file from server

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const stuffRoutes = require("./routes/stuff");
 const userRoutes = require('./routes/user');
@@ -27,6 +28,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
